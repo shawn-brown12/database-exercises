a537a03f7c62fe34ffc916b31493b03a50f0b56d
Shawn.Brown <shawn.c.brown12@gmail.com>
2022-10-24 10:33:49 -0500

    Updated mysql_introduction file

--- mysql_introduction.sql
+++ mysql_introduction.sql
@@ -1,7 +1,11 @@
-SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;;
-SELECT * FROM mysql.help_topic;;
-SELECT user, host FROM mysql.user;;
-SELECT * FROM mysql.user;;
-SHOW CREATE TABLE orders;;
-USE chipotle;;
 SHOW DATABASES;
+
+SELECT user, host FROM mysql.user;
+
+SELECT * FROM mysql.user;
+
+SELECT user, host FROM mysql.user;
+
+SELECT * FROM mysql.help_topic;
+
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;
