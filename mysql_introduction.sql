644d18bbd74af9de82eff8754ea5c496882c35ad
Shawn.Brown <shawn.c.brown12@gmail.com>
2022-10-24 10:23:01 -0500

    Added initial SQL Database queries

---
+++ mysql_introduction.sql
@@ -0,0 +1,7 @@
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic;;
+SELECT * FROM mysql.help_topic;;
+SELECT user, host FROM mysql.user;;
+SELECT * FROM mysql.user;;
+SHOW CREATE TABLE orders;;
+USE chipotle;;
+SHOW DATABASES;
