3e68835605b84c464fc814e093ce1a532eddf147
Shawn.Brown <shawn.c.brown12@gmail.com>
2022-10-27 13:37:43 -0500

    Corrected a mistake and cleaned up code a bit

--- join_exercises.sql
+++ join_exercises.sql
@@ -29,7 +29,7 @@ FROM titles
 	JOIN employees USING(emp_no)
 	JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 	JOIN departments ON dept_emp.dept_no = departments.dept_no
-WHERE dept_emp.to_date = '9999-01-01' AND titles.to_date = '9999-01-01';
+WHERE dept_emp.to_date = '9999-01-01' AND titles.to_date = '9999-01-01' AND dept_name = 'Customer Service';
 
 #Q5 Find the current salary of all current managers.
 
@@ -49,13 +49,14 @@ GROUP BY dept_name;
 
 #Q7 Which department has the highest average salary? Hint: Use current not historic information.
 
-SELECT dept_name, AVG(salary) AS avg_salary
+SELECT dept_name, ROUND(AVG(salary), 2) AS avg_salary
 FROM departments
 	JOIN dept_emp USING(dept_no)
 	JOIN salaries USING(emp_no)
-WHERE salaries.to_date = '9999-01-01'
+WHERE salaries.to_date > CURDATE()
 GROUP BY dept_name
-ORDER BY  avg_salary DESC;
+ORDER BY  avg_salary DESC
+LIMIT 1;
 
 #Q8 Who is the highest paid employee in the Marketing department?
 
@@ -65,7 +66,7 @@ FROM dept_emp
 	JOIN employees USING(emp_no)
 	JOIN salaries USING(emp_no)
 WHERE departments.dept_name = 'Marketing' AND dept_emp.to_date = '9999-01-01'
-GROUP BY full_name
+GROUP BY dept_name, full_name
 ORDER BY max_salary DESC
 LIMIT 1;
 
