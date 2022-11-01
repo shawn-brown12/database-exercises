bcd22ef287381c8230a154dbe3018f5cec3d9237
Shawn.Brown <shawn.c.brown12@gmail.com>
2022-11-01 09:53:41 -0500

    finished last exercise and updated some syntax

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -12,7 +12,9 @@ WHERE dept_emp.to_date > CURDATE()
 SELECT *
 FROM noether_2030.employees_with_departments;
 
-DROP TABLE IF EXISTS noether_2030.employees_with_departments;
+DROP TEMPORARY TABLE IF EXISTS noether_2030.employees_with_departments;
+
+DESCRIBE noether_2030.employees_with_departments;
 
 #A Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
 ALTER TABLE noether_2030.employees_with_departments ADD full_name VARCHAR(32);
@@ -29,7 +31,7 @@ DROP COLUMN last_name;
 
 #D What is another way you could have ended up with this same table?
 CREATE TEMPORARY TABLE noether_2030.employees_and_departments AS (
-SELECT CONCAT(first_name, ' ', last_name) to_date, dept_name
+SELECT CONCAT(first_name, ' ', last_name) AS full_name, to_date, dept_name
 FROM employees
 JOIN dept_emp AS de USING(emp_no)
 JOIN departments AS d USING(dept_no)
@@ -66,13 +68,32 @@ SELECT salary,
     (SELECT stddev(salary) FROM salaries) AS zscore
 FROM salaries;
 */
-SELECT dept_name, AVG(salary)
-FROM dept_emp
+									  
+CREATE TEMPORARY TABLE noether_2030.salary_info AS (
+SELECT DISTINCT dept_name, AVG(salary)
+FROM departments AS d
+JOIN dept_emp AS de USING(dept_no)
+JOIN employees AS e USING(emp_no)
 JOIN salaries AS s USING(emp_no)
-JOIN departments AS d USING(dept_no)
-WHERE dept_emp.to_date > CURDATE()
-GROUP BY dept_name;
+WHERE s.to_date > CURDATE() AND de.to_date > CURDATE()
+GROUP BY dept_name
+);
 
+SELECT *
+FROM noether_2030.salary_info;
+
+SELECT *
+FROM noether_2030.avg_and_std;
+
+CREATE TEMPORARY TABLE noether_2030.avg_and_std AS (
+SELECT AVG(salary) AS avg_salary, STD(salary) AS std_salary
+FROM salaries
+WHERE to_date > CURDATE()
+);
+
+DROP TABLE IF EXISTS noether_2030.salary_info;
+
+/* 
 CREATE TEMPORARY TABLE noether_2030.dept_avg_sal AS (
 SELECT dept_name, AVG(s.salary), zscore
 FROM dept_emp
@@ -86,13 +107,57 @@ JOIN departments AS d USING(dept_no)
 WHERE dept_emp.to_date > CURDATE()
 GROUP BY dept_name, zscore
 );
+*/
+
+-- Adam's example solution
+
+#Equation for zscore
+SELECT salary,
+    (salary - (SELECT AVG(salary) FROM salaries))
+    /
+    (SELECT stddev(salary) FROM salaries) AS zscore
+FROM salaries;
+
+#Creating table with avg and std
+CREATE TEMPORARY TABLE noether_2030.overall_agg AS (
+	SELECT AVG(salary) AS avg_salary, STD(salary) AS std_salary
+	 FROM employees.salaries
+	 WHERE to_date > NOW()
+);
+
+#Checking values
+SELECT *
+FROM noether_2030.overall_agg;
+
+#Average salary by dept
+CREATE TEMPORARY TABLE noether_2030.metrics AS (
+	SELECT dept_name, AVG(salary) AS dept_average
+	FROM employees.salaries
+	JOIN employees.dept_emp USING(emp_no)
+	JOIN employees.departments USING(dept_no)
+	WHERE employees.dept_emp.to_date > NOW()
+	AND employees.salaries.to_date > NOW()
+	GROUP BY dept_name
+);
 
 SELECT *
-FROM noether_2030.dept_avg_sal;
+FROM noether_2030.metrics;
 
-DROP TABLE IF EXISTS noether_2030.dept_avg_sal;
-ALTER TABLE noether_2030.dept_avg_sal 
-ALTER TABLE noether_2030.dept_avg_sal ADD zscore FLOAT;
-ALTER TABLE noether_2030.dept_avg_sal SET zscore = salary;
+#Create columns for table
+ALTER TABLE noether_2030.metrics ADD overall_avg FLOAT (10, 2);
+ALTER TABLE noether_2030.metrics ADD overall_std FLOAT (10, 2);
+ALTER TABLE noether_2030.metrics ADD dept_zscore FLOAT (10, 2);
 
+#Update table with avg
+UPDATE noether_2030.metrics SET overall_avg = (SELECT avg_salary FROM noether_2030.overall_agg);
+
+#Update table with std
+UPDATE noether_2030.metrics SET overall_std = (SELECT std_salary FROM noether_2030.overall_agg);
+
+#Update with zscore
+UPDATE noether_2030.metrics SET dept_zscore = (SELECT dept_average - overall_avg) / overall_std;
+
+#Final check
+SELECT *
+FROM noether_2030.metrics;
 #BONUS To your work with current salary zscores, determine the overall historic average departement average salary, the historic overall average, and the historic zscores for salary. Do the zscores for current department average salaries tell a similar or a different story than the historic department salary zscores?
